d919d555c29644aedb5d5ab13ef1c82b4b8ca24c
the-braveknight <zaid.riadh@gmail.com>
2026-08-15 12:28:13 +0300

    Add Entitlements protobuf contract

--- Package.swift
+++ Package.swift
@@ -10,27 +10,42 @@ let package = Package(
     ],
     products: [
         .library(
+            name: "EntitlementsProtos",
+            targets: ["EntitlementsProtos"]
+        ),
+        .library(
             name: "NewsletterProtos",
             targets: ["NewsletterProtos"]
-        )
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift-2.git", from: "2.4.0"),
         .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "2.4.0"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.32.0")
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.32.0"),
     ],
     targets: [
+        .target(
+            name: "EntitlementsProtos",
+            dependencies: [
+                .product(name: "GRPCCore", package: "grpc-swift-2"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ],
+            plugins: [
+                .plugin(name: "GRPCProtobufGenerator", package: "grpc-swift-protobuf")
+            ]
+        ),
         .target(
             name: "NewsletterProtos",
             dependencies: [
                 .product(name: "GRPCCore", package: "grpc-swift-2"),
                 .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
             ],
             plugins: [
                 .plugin(name: "GRPCProtobufGenerator", package: "grpc-swift-protobuf")
             ]
-        )
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
