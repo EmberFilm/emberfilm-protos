ad1e3f677757e8a89cb3305fe89b9244e0917602
the-braveknight <zaid.riadh@gmail.com>
2026-08-15 04:00:50 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "emberfilm-protos",
+    platforms: [
+        .macOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "NewsletterProtos",
+            targets: ["NewsletterProtos"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift-2.git", from: "2.4.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "2.4.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.32.0")
+    ],
+    targets: [
+        .target(
+            name: "NewsletterProtos",
+            dependencies: [
+                .product(name: "GRPCCore", package: "grpc-swift-2"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ],
+            plugins: [
+                .plugin(name: "GRPCProtobufGenerator", package: "grpc-swift-protobuf")
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ Sources/NewsletterProtos/NewsletterProtos.swift
@@ -0,0 +1,6 @@
+//
+//  NewsletterProtos.swift
+//  emberfilm-protos
+//
+//  Created by Zaid Rahhawi on 8/15/26.
+//
